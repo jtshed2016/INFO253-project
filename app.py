fe509b0054fea2f7f445d1ad1b8f8b56892ed96d
jtshed2016 <jshedlock@berkeley.edu>
2015-12-10 22:38:11 -0800

    First upload of project part 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#libraries, etc. from project skeleton code
 import shelve
 from subprocess import check_output
 import flask
@@ -9,62 +10,144 @@ import os
 import sys
 import json
 import hashlib
+from urllib.request import urlopen
 
 app = flask.Flask(__name__)
 app.debug = True
 
-# Database/Dictionary to save shortened URLs
-db = shelve.open("shorten.db")
+#Create database/dictionary to store records (or reopen, if already existing)
+db = shelve.open("msdescriptions", flag="c")
+
+#load initial records from JSON file if not already populated
+#not all records have all fields; populate missing values with 'N/A' to prevent key errors
+print(len(db))
+values = ['Author', 'Title', 'Place', 'Language', 'Description']
+if len(db)==0:
+	sourcefile = open('newtext_short.json', 'r')
+	initialrecords=json.load(sourcefile)
+	#print(initialrecords)
+	for record in initialrecords:
+		print(record)
+		tempholder = {}
+		for fieldtitle in values:
+			if fieldtitle in initialrecords[record]:
+				print(fieldtitle, initialrecords[record][fieldtitle])
+				tempholder[fieldtitle] = initialrecords[record][fieldtitle]
+				#print(db[record][fieldtitle])
+			else:
+				print(fieldtitle, 'not here')
+				tempholder[fieldtitle] = 'N/A'
+				#print(db[record][fieldtitle])
+		db[record] = tempholder
+		print('\n')
+	
+	#for record in initialrecords:
+		#print('*',record)
+		#print(initialrecords[record])
+	#	db[record] = {}
+		#print(db[record])
+	#	for fieldtitle in values:
+			#print('**',fieldtitle)
+	#		if fieldtitle in initialrecords[record]:
+				#print('***',initialrecords[record][fieldtitle])
+	#			db[record][fieldtitle] = initialrecords[record][fieldtitle]
+				#print(db[record][fieldtitle])
+	#		else:
+	#			db[record][fieldtitle] = 'N/A'
+				#print('*** Ruh-roh')
+		#print(db[record])
+	sourcefile.close()
+	#print('DB')
+	for x in db:
+		print(x, db[x], len(db[x]))
+
+#load API keys
+key_googlemaps = 'AIzaSyD9rloC3_q9b1ZwhocaDBpuXaXyJeZkKjk'
+key_geonames = 'jtshed_geo2015'
 
 @app.route('/')
 def index():
     """
-    Builds a template based on a GET request, with some default
-    arguments
+    Main landing page with links to view, add pages.
     """
     
 
-    return flask.render_template('index.html')
+    return flask.render_template('home.html')
 
-###
-# Now we'd like to do this generally:
-# <short> will match any word and put it into the variable =short= Your task is
-# to store the POST information in =db=, and then later redirect a GET request
-# for that same word to the URL provided.  If there is no association between a
-# =short= word and a URL, then return a 404
-###
 
-@app.route("/create", methods=['POST'])
+@app.route("/addrecord", methods=['GET'])
+def addpage():
+	return flask.render_template('newrecord.html')
+
+@app.route("/newrecord", methods=['POST'])
 def create():
     """
-    This POST request creates an association between a short url and a full url
-    and saves it in the database (the dictionary db)
+    Add new record to site, visit successpage
     """
     
-    target_url = request.form['input_url']
-    shortened_url = request.form['input_short']
-    db[shortened_url] = target_url
+    identifier = request.form['identifier']
+    title = request.form['mstitle']
+    author = request.form['msauthor']
+    place = request.form['msplace']
+    lang = request.form['mslang']
+    description = request.form['msdescription']
+    
+    db[identifier] = {'Title': title, 'Author': author, 'Place': place, 'Language': lang, 'Description': description}
     
     
-    return flask.render_template('success.html', target=target_url, shortened=shortened_url, tfromdb = db[shortened_url])
+    return flask.render_template('success.html', url_id=identifier, success_title=title)
     #raise NotImplementedError     
 
-@app.route("/short/<short>", methods=['GET'])
-def point_to_url(short):
+@app.route("/record/<shelfmark>", methods=['GET'])
+def point_to_url(shelfmark):
     """
-    Redirect the request to the URL associated =short=, otherwise return 404
-    NOT FOUND
+    Visit the site of a specific record
     """
-    if short in db:
-    	return redirect(db[short], code=302)
+    recordinfo={}
+    fields = ['Title', 'Author', 'Language', 'Place', 'Description']
+    if shelfmark in db:
+    	for field in fields:
+    		if field in db[shelfmark]:
+    			recordinfo[field] = db[shelfmark][field]
+    		else:
+    			recordinfo[field] = 'N/A'
+    	recordinfo['lat'], recordinfo['lon'] = get_coords(recordinfo['Place'])
+    	
+    	#print(recordinfo['lat'], recordinfo['lon'])
+    			
+    		
+    			
+    			
+    	return flask.render_template('recordpage.html', identifier=shelfmark, title=recordinfo['Title'], place=recordinfo['Place'], language=recordinfo['Language'], desc=recordinfo['Description'], author=recordinfo['Author'], originlat=recordinfo['lat'], originlon=recordinfo['lon'], googleapi=key_googlemaps)
     else:
     	abort(404)
     
     #raise NotImplementedError 
+    
+@app.route("/msslist", methods=['GET'])
+def showtestpage():
+	valuesfromdb = {}
+	for x in db:
+		valuesfromdb[x] = db[x]
+	
+	return render_template('listpage.html', data=valuesfromdb )
+	
+@app.route("/json", methods = ['GET'])
+def send_db_in_json():
+	return flask.jsonify(**db)
 
 @app.errorhandler(404)
 def nopath(error):
 	return render_template('notfound.html'), 404
+	
+	
+def get_coords(placename):
+	#Sends place name from record to Geonames API, retrieves latitude and longitude
+	namequery = 'http://api.geonames.org/searchJSON?q=' + placename + '&username=' + key_geonames
+	namerecords = urlopen(namequery).read().decode("utf-8")
+	resultsdict = json.loads(namerecords)
+	return resultsdict['geonames'][0]['lat'], resultsdict['geonames'][0]['lng']
+	
 
 if __name__ == "__main__":
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,123 @@
 //define a regular expression pattern to test for letters
 var allLetters = /^[A-Za-z]+$/;
+var newRecordFields = [0,0,0,0,0,0]
+//flags for whether new records are filled in: 0-shelfmark/identifier, 1-title,
+//2-author, 3-place, 4-language, 5-description
+
+function makeTableFromJson(recordObject) {
+	console.log(typeof(recordObject));
+	var tableText = '<table id="mssTable" cellspacing=2 cellpadding=2 border=1>'
+	tableText+= '<tr><th>Shelfmark</th><th>Author</th><th>Title</th></tr>'
+	
+	$.each(recordObject, function(k,v) {
+		tableText += '<tr><td><a href="/record/' + k +'">' + k + '</td><td>' + v.Author + '</td><td>' + v.Title +'</td></tr>';
+	})
+
+	tableText+='</table>'
+	console.log(tableText)
+	$("#mssinfotable").html(tableText);
+}
 
 $(document).ready(function() {
+	if ($("#titlediv p").text() === 'N/A') {
+		$("titlediv").css('display', 'none');
+		console.log("success");
+	}
+
+
+
+
+
+$( "#input_short").keyup(function() {
+		if (allLetters.test($("#input_short").val()) === true) {
+			//if all characters are alpha, activate and color submit button
+			$("#saveurl").prop('disabled', false);
+			$("#saveurl").css('color', 'red');
+			$("#saveurl").css('background-color', 'rgb(102, 102, 153)');
+			
+		}
+		else {
+			//if string is empty or not completely alphabetical, disable and grey out button
+			$("#saveurl").prop('disabled', true);
+			$("#saveurl").css('color', 'grey');
+			$("#saveurl").css('background-color', 'rgb(209, 209, 224)');
+		}
+	});
+	
+
+//check each field for blankness; set flag to 1 or 0
+
+	$("#identifier").keyup(function() {
+		if ($("#identifier").val() === '') {
+			newRecordFields[0] = 0;
+		}
+		else {
+			newRecordFields[0] = 1;
+		}
+	});
+	
+	$("#mstitle").keyup(function() {
+		if ($("#mstitle").val() === '') {
+			newRecordFields[1] = 0;
+		}
+		else {
+			newRecordFields[1] = 1;
+		}
+	});
+	
+	$("#msauthor").keyup(function() {
+		if ($("#msauthor").val() === '') {
+			newRecordFields[2] = 0;
+		}
+		else {
+			newRecordFields[2] = 1;
+		}
+	});
+	
+	$("#msplace").keyup(function() {
+		if ($("#msplace").val() === '') {
+			newRecordFields[3] = 0;
+		}
+		else {
+			newRecordFields[3] = 1;
+		}
+	});
+	
+	$("#mslang").keyup(function() {
+		if ($("#mslang").val() === '') {
+			newRecordFields[4] = 0;
+		}
+		else {
+			newRecordFields[4] = 1;
+		}
+	});
+	
+	$("#msdescription").keyup(function() {
+		if ($("#msdescription").val() === '') {
+			newRecordFields[5] = 0;
+		}
+		else {
+			newRecordFields[5] = 1;
+		}
+	});
+
+
+//on each keystroke, check whether each field is blank (based on values above) and enable or disable submit button accordingly
+	$("#newrecordform").keyup(function() {
+		//console.log(newRecordFields);
+		counter = 0;
+		for (i=0; i < newRecordFields.length; i++) {
+			counter = counter + newRecordFields[i];
+		}
+		//console.log(counter);
+		if (counter===6) {
+			$("#recordsubmit").prop('disabled', false);
+		}
+		else {
+			$("#recordsubmit").prop('disabled', true);
+		}
+	});
+
 
 	//function to detect input into short URL field and activate if alpha
 	$( "#input_short").keyup(function() {
@@ -20,4 +136,11 @@ $(document).ready(function() {
 		}
 	});
 	
+
+	
+	//$("#titlediv").ready(function() {
+	//	if ($("#titlediv p").text() === 'N/A') {
+	//		$("#titlediv").css('display', 'none');
+	//	}
+	//});
 });
